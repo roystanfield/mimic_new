3836d519f01b334bd1561644e7077722119a5d2d
Roy Stanfield <roystanfield@gmail.com>
2014-07-25 12:26:31 -0400

    Colorful version of webpage.

--- app/assets/javascripts/jquery_carousel.js
+++ app/assets/javascripts/jquery_carousel.js
@@ -3,16 +3,22 @@ $(document).ready(function() {
   // Set up
   var imagesArray = [];
   var appIconsArray = [];
+  var dictionary = [];
   var currentVisibleScreensIndex = 0;
+
+  dictionary.push({key: 0, value: "etsy"});
+  dictionary.push({key: 1, value: "fb"});
+  dictionary.push({key: 2, value: "capsule"});
+  dictionary.push({key: 3, value: "mixel"});
 
   imagesArray = $(".image_wrapper").find("li");
   appIconsArray = $(".app_icon_wrapper").find("li");
   hideAllImages(imagesArray);
   hideAllDots(appIconsArray);
+
   $(imagesArray[0]).removeClass("hide").addClass("transition");
   $(appIconsArray[0]).removeClass("hide").addClass("transition");
   $(appIconsArray[0]).find("span").removeClass("hide");
-
 
 
   // Listen for clicks on app icons
@@ -44,6 +50,11 @@ $(document).ready(function() {
 
 
   function showCorrectScreen(appsIndex, currentVisibleScreensIndex) {
+
+    var addThisClass = dictionary[appsIndex].value;
+    var removeThisClass = dictionary[currentVisibleScreensIndex].value;
+    $('body').addClass(addThisClass).removeClass(removeThisClass);
+
     $(imagesArray[currentVisibleScreensIndex]).addClass("hide").removeClass("transition");
     $(appIconsArray[currentVisibleScreensIndex]).find("span").addClass("hide");
     $(imagesArray[appsIndex]).removeClass("hide").addClass("transition");
