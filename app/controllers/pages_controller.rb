8ec2363b655eeca99aa4ae7b18b3b9e3841d9271
Roy Stanfield <roystanfield@gmail.com>
2015-11-01 22:41:15 -0500

    Polishing up the portfolio.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  http_basic_authenticate_with name: "raybarbee", password: "minirat"
-  
+  http_basic_authenticate_with name: "raybarbee", password: "minirats"
+
   def etsy_native
   end
 end
